5ae67163c9977df757fe620ba9c8a7e1d87a9f2c
James Steadham <jesteadham@gmail.com>
2022-06-21 07:16:10 -0400

    updated configurations to include babel version 7+

--- embrace/src/App.js
+++ embrace/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <h2 className="underline bg-gray-200">Welcome to React</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </>
+  );
 }
 
 export default App;
